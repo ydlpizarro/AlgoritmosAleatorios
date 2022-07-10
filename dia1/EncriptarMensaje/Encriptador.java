1a7f0e3c6ffafb350cbf744fb3af65871e5acc7b
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2022-07-09 22:21:29 -0300

    correcion uso de parametro llave en metodos MensajeSecreta

--- dia1/EncriptarMensaje/Encriptador.java
+++ dia1/EncriptarMensaje/Encriptador.java
@@ -1,21 +1,21 @@
 package dia1.EncriptarMensaje;
 
 public class Encriptador {
-    public StringBuilder CodificarMensaje(String mensajeOriginal, int llave) {
+    public StringBuilder CodificarMensaje(String mensajeOriginal, int llaveCodificarMensaje) {
         StringBuilder output = new StringBuilder();
         int valorCaracter;
         for (int index = 0; index < mensajeOriginal.length(); index++) {
-            valorCaracter = mensajeOriginal.charAt(index) + 1;
+            valorCaracter = mensajeOriginal.charAt(index) + llaveCodificarMensaje;
             output.append((char) valorCaracter);
         }
         return output;
     }
 
-    public StringBuilder DecodificarMensaje(String mensajeEncriptada, int llave) {
+    public StringBuilder DecodificarMensaje(String mensajeEncriptada, int llaveDecodificarMensaje) {
         StringBuilder output = new StringBuilder();
         int valorCaracter;
         for (int index = 0; index < mensajeEncriptada.length(); index++) {
-            valorCaracter = mensajeEncriptada.charAt(index) - 1;
+            valorCaracter = mensajeEncriptada.charAt(index) - llaveDecodificarMensaje;
             output.append((char) valorCaracter);
         }
         return output;
--- dia1/EncriptarMensaje/MensajeSecreta.java
+++ dia1/EncriptarMensaje/MensajeSecreta.java
@@ -4,8 +4,13 @@ public class MensajeSecreta {
     public static void main(String[] args) {
         String mensajeOriginal = "Hola Como Estas";
         String mensajeEncriptada = "Ipmb!Dpnp!Ftubt";
+
+        int llaveCodificar = 1;
+        int llaveDecodificar = 1;
+
         Encriptador encriptador = new Encriptador();
-        System.out.println(encriptador.CodificarMensaje(mensajeOriginal, 1));
-        System.out.println(encriptador.DecodificarMensaje(mensajeEncriptada, 1));
+
+        System.out.println(encriptador.CodificarMensaje(mensajeOriginal, llaveCodificar));
+        System.out.println(encriptador.DecodificarMensaje(mensajeEncriptada, llaveDecodificar));
     }
 }
